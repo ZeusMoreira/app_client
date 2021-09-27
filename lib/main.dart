db1694e7c11f771c98c098b79d3048a17a0f8721
VitorLameirao <vitorlameirao10@gmail.com>
2021-09-27 09:55:19 -0300

    Adicionado Logo Pato Burguer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -27,12 +27,51 @@ class HomePageState extends State<HomePage>{
               color: Colors.white,
             ),
             borderRadius: BorderRadius.only(
-              bottomRight: Radius.circular(40.0),
-              bottomLeft: Radius.circular(40.0)
+              bottomRight: Radius.circular(20.0),
+              bottomLeft: Radius.circular(20.0)
             )
           ),
           height: 572.0,
           width: 360.0,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                  top: 29.0,
+                  bottom: 513.0,  //Original é 516, porém estava erro de Render
+                  left: 118.0,
+                  right: 90.0
+                ),
+                child: RichText(
+                  text: TextSpan(
+                    text: "Pato",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      color: Color(0xFF333333),
+                      fontFamily: "Roboto",
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.w900
+                  ),
+                  children: [
+                    TextSpan(
+                      text: " Burguer",
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        color: Color(0xFFFF9B0D),
+                        fontFamily: "Roboto",
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w900
+                      )
+                    )
+                  ]
+                  )
+                )
+                )
+              ],),
+            ],),
         ),
             
         )
