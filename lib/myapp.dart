19d7c87b8f2f447f54d3349cef7931dc35352f21
VitorLameirao <vitorlameirao10@gmail.com>
2021-09-28 10:29:20 -0300

    Adicionado BottomNaviBar

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'package:app_client/bottomnavi.dart';
 import 'package:app_client/design.dart';
 import 'package:app_client/assets/pb_icons.dart';
 import 'package:flutter/material.dart';
@@ -19,31 +20,7 @@ class _MyAppState extends State<MyApp>{
       home: Scaffold(
         backgroundColor: Color(0xFFFF9B0D),
         body: Design(),
-        bottomNavigationBar: SizedBox(
-          height: 70.0,
-          child: BottomNavigationBar(
-            unselectedItemColor: Colors.grey,
-            selectedItemColor: Color(0xFFFFFFFF),
-            iconSize: 30.0,
-            elevation: 0.0,
-            backgroundColor: Color(0xFFFF9B0D),
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                  color: Color(0xFFFFFFFF),
-                  ),
-                  label: "Home"
-                  ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.info_sharp,
-                  color: Color(0xFFFFFFFF),
-                  ),
-                  label: "Sobre Nós"
-                  )
-            ],),
-        ),
+        bottomNavigationBar: BottomNavi(),
       )
       );
   }
